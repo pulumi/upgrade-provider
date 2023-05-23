e4c5e97bc23f251597a6092db046257e5f49a90c
aq17 <aqiu@pulumi.com>
2023-05-23 12:02:48 -0700

    Add flag to remove plugins

--- upgrade/upgrade-provider.go
+++ upgrade/upgrade-provider.go
@@ -263,10 +263,15 @@ func UpgradeProvider(ctx Context, repoOrg, repoName string) error {
 		}
 	}
 
+	addPluginStep := step.Cmd(exec.CommandContext(ctx, "pulumi", "plugin", "rm", "--all", "--yes"))
+	if !ctx.RemovePlugins {
+		addPluginStep = step.Cmd(exec.Command("echo", "Plugins not removed."))
+	}
+
 	artifacts := append(steps,
 		step.Cmd(exec.CommandContext(ctx, "go", "mod", "tidy")).In(repo.providerDir()),
 		step.Cmd(exec.CommandContext(ctx, "go", "mod", "tidy")).In(repo.examplesDir()),
-		step.Cmd(exec.CommandContext(ctx, "pulumi", "plugin", "rm", "--all", "--yes")),
+		addPluginStep,
 		step.Cmd(exec.CommandContext(ctx, "make", "tfgen")).In(&repo.root),
 		step.Cmd(exec.CommandContext(ctx, "git", "add", "--all")).In(&repo.root),
 		GitCommit(ctx, "make tfgen").In(&repo.root),
