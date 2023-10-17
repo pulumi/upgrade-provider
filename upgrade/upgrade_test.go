c510be470dfecb059958cca9f0cd07ffda3197b4
Ian Wahbe <ian@wahbe.com>
2023-10-17 16:13:28 -0700

    Move replay test data to it's own file

--- upgrade/upgrade_test.go
+++ upgrade/upgrade_test.go
@@ -2,6 +2,8 @@ package upgrade
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/Masterminds/semver/v3"
@@ -11,101 +13,16 @@ import (
 )
 
 func TestInformGithub(t *testing.T) {
-	replay := step.NewReplay(t, []byte(`{
-        "pipelines": [
-            {
-                "name": "Tfgen & Build SDKs",
-                "steps": [
-                    {
-                        "name": "Inform Github",
-                        "inputs": [
-                            {
-                                "Version": "5.0.5",
-                                "GHIssues": [
-                                    {
-                                        "number": 232
-                                    }
-                                ]
-                            },
-                            {},
-                            {
-                                "Kind": "plain",
-                                "Upstream": {
-                                    "Path": "github.com/vmware/terraform-provider-wavefront",
-                                    "Version": "v0.0.0-20231006183745-aa9a262f8bb0"
-                                },
-                                "Fork": null,
-                                "Bridge": {
-                                    "Path": "github.com/pulumi/pulumi-terraform-bridge/v3",
-                                    "Version": "v3.61.0"
-                                },
-                                "Pf": {
-                                    "Path": ""
-                                },
-                                "UpstreamProviderOrg": "vmware"
-                            },
-                            null,
-                            null,
-                            "Up to date at 2.29.0",
-                            ["upgrade-provider", "pulumi/pulumi-wavefront"]
-                        ],
-                        "outputs": [
-                            null
-                        ]
-                    },
-                    {
-                        "name": "git",
-                        "inputs": [
-                            "git",  ["push", "--set-upstream", "origin", "upgrade-terraform-provider-wavefront-to-v5.0.5"]
-                        ],
-                        "outputs": [
-                            "branch 'upgrade-terraform-provider-wavefront-to-v5.0.5' set up to track 'origin/upgrade-terraform-provider-wavefront-to-v5.0.5'.\n",
-                            null
-                        ],
-                        "impure": true
-                    },
-                    {
-                        "name": "gh",
-    "inputs": [
-      "gh",  ["pr", "create", "--assignee", "@me", "--base", "master", "--head", "upgrade-terraform-provider-wavefront-to-v5.0.5", "--reviewer", "pulumi/Providers,lukehoban", "--title", "Upgrade terraform-provider-wavefront to v5.0.5", "--body", "This PR was generated via `+"`$ upgrade-provider pulumi/pulumi-wavefront`"+`.\n\n---\n\n- Upgrading terraform-provider-wavefront from 5.0.3  to 5.0.5.\n\tFixes #232\n"]
-    ],
-    "outputs": [
-      "https://github.com/pulumi/pulumi-wavefront/pull/239\n",
-      null
-    ],
-    "impure": true
-  },
-  {
-    "name": "Assign Issues",
-    "inputs": [],
-    "outputs": [
-      null
-    ]
-  },
-  {
-    "name": "gh",
-    "inputs": [
-      "gh", ["issue", "edit", "232", "--add-assignee", "@me"]
-    ],
-    "outputs": [
-      "https://github.com/pulumi/pulumi-wavefront/issues/232\n",
-      null
-    ],
-    "impure": true
-  }
-]
-}
-]
-}`))
+	ctx := newReplay(t, "wavefront-inform-github")
 
-	ctx := (&Context{
+	ctx = (&Context{
 		UpgradeProviderVersion: true,
 		PrAssign:               "@me",
 		PrReviewers:            "pulumi/Providers,lukehoban",
 		UpstreamProviderName:   "terraform-provider-wavefront",
-	}).Wrap(context.Background())
+	}).Wrap(ctx)
 
-	err := step.PipelineCtx(step.WithEnv(ctx, replay), "Tfgen & Build SDKs", func(ctx context.Context) {
+	err := step.PipelineCtx(ctx, "Tfgen & Build SDKs", func(ctx context.Context) {
 		step.Call70E(ctx, "Inform Github", InformGitHub,
 			&UpstreamUpgradeTarget{
 				GHIssues: []UpgradeTargetIssue{
@@ -131,4 +48,18 @@ func TestInformGithub(t *testing.T) {
 			}, nil, nil, "Up to date at 2.29.0", []string{"upgrade-provider", "pulumi/pulumi-wavefront"})
 	})
 	require.NoError(t, err)
+}
+
+func newReplay(t *testing.T, name string) context.Context {
+	ctx := context.Background()
+	path := filepath.Join("testdata", "replay", name+".json")
+	bytes := readFile(t, path)
+	r := step.NewReplay(t, bytes)
+	return step.WithEnv(ctx, r)
+}
+
+func readFile(t *testing.T, path string) []byte {
+	bytes, err := os.ReadFile(path)
+	require.NoError(t, err)
+	return bytes
 }
