pulumi/upgrade-provider

commit 9dbeae554475dba6b436d2b6ae288118b6f4b0ba

Hide *Context inside context.Context (#174)
This allows multiple systems to pass context.Context without
interference, normalizing function signatures. This is a pure refactor
and does not have user facing changes.

upgrade/kind.go
@@ -1,6 +1,7 @@
 package upgrade
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -78,7 +79,7 @@ func (rk RepoKind) IsPatched() bool {
 	}
 }
 
-func GetRepoKind(ctx Context, repo ProviderRepo) (*GoMod, error) {
+func GetRepoKind(ctx context.Context, repo ProviderRepo) (*GoMod, error) {
 	path := repo.root
 	file := filepath.Join(path, "provider", "go.mod")
 
@@ -104,11 +105,11 @@ func GetRepoKind(ctx Context, repo ProviderRepo) (*GoMod, error) {
 	if err != nil {
 		return nil, err
 	}
-	if !ok && ctx.UpgradeProviderVersion {
+	if ctx := GetContext(ctx); !ok && ctx.UpgradeProviderVersion {
 		setFalse(&ctx.UpgradeProviderVersion)
 	}
 
-	tfProviderRepoName := ctx.UpstreamProviderName
+	tfProviderRepoName := GetContext(ctx).UpstreamProviderName
 
 	getUpstream := func(file *modfile.File) (*modfile.Require, error) {
 		// Find the name of our upstream dependency

main.go
@@ -42,10 +42,8 @@ func cmd() *cobra.Command {
 	var repoOrg string
 	var repoPath string
 
-	context := upgrade.Context{
-		Context: context.Background(),
-		GoPath:  gopath,
-	}
+	ctx := context.Background()
+	context := upgrade.Context{GoPath: gopath}
 
 	exitOnError := func(err error) {
 		if err == nil {
@@ -163,7 +161,7 @@ func cmd() *cobra.Command {
 		},
 		Run: func(_ *cobra.Command, args []string) {
 			exitOnError(failedPreRun)
-			err := upgrade.UpgradeProvider(context, repoOrg, repoName)
+			err := upgrade.UpgradeProvider(context.Wrap(ctx), repoOrg, repoName)
 			if err != nil && context.CreateFailureIssue {
 				// $GITHUB_ACTION is a default env var within github
 				// actions, but is unlikely to be defined elsewhere.
@@ -171,7 +169,7 @@ func cmd() *cobra.Command {
 				// We do this to test that we are being run inside a GH
 				// action.
 				if _, ci := os.LookupEnv("GITHUB_ACTION"); ci {
-					msg, err := createFailureIssue(context, repoOrg, repoName)
+					msg, err := createFailureIssue(ctx, repoOrg, repoName)
 					if err != nil {
 						fmt.Println(msg)
 					}
@@ -305,7 +303,7 @@ func bindFlags(cmd *cobra.Command, v *viper.Viper) {
 }
 
 // Create an issue in the provider repo with a message describing the upgrade failure
-func createFailureIssue(ctx upgrade.Context, repoOrg string, repoName string) (string, error) {
+func createFailureIssue(ctx context.Context, repoOrg string, repoName string) (string, error) {
 	now := time.Now()
 	hr, min, _ := now.Clock()
 	yr, mth, day := now.Date()

upgrade/steps-helpers.go
@@ -115,7 +115,7 @@ func baseFileAt(ctx context.Context, repo ProviderRepo, file string) ([]byte, er
 	return data, nil
 }
 
-func prBody(ctx Context, repo ProviderRepo,
+func prBody(ctx context.Context, repo ProviderRepo,
 	upgradeTarget *UpstreamUpgradeTarget, goMod *GoMod,
 	targetBridge, tfSDKUpgrade string) string {
 	b := new(strings.Builder)
@@ -124,10 +124,10 @@ func prBody(ctx Context, repo ProviderRepo,
 
 	fmt.Fprintf(b, "\n---\n\n")
 
-	if ctx.MajorVersionBump {
+	if GetContext(ctx).MajorVersionBump {
 		fmt.Fprintf(b, "- Updating major version from %s to %s.\n", repo.currentVersion, repo.currentVersion.IncMajor())
 	}
-	if ctx.oldJavaVersion != ctx.JavaVersion && ctx.JavaVersion != "" {
+	if ctx := GetContext(ctx); ctx.oldJavaVersion != ctx.JavaVersion && ctx.JavaVersion != "" {
 		var from string
 		if prev := ctx.oldJavaVersion; prev != "" {
 			from = fmt.Sprintf("from %s ", prev)
@@ -135,30 +135,30 @@ func prBody(ctx Context, repo ProviderRepo,
 		fmt.Fprintf(b, "- Updating java version %sto %s.\n", from, ctx.JavaVersion)
 	}
 
-	if ctx.UpgradeProviderVersion {
+	if GetContext(ctx).UpgradeProviderVersion {
 		contract.Assertf(upgradeTarget != nil, "upgradeTarget should always be non-nil")
 		var prev string
 		if repo.currentUpstreamVersion != nil {
 			prev = fmt.Sprintf("from %s ", repo.currentUpstreamVersion)
 		}
 		fmt.Fprintf(b, "- Upgrading %s %s to %s.\n",
-			ctx.UpstreamProviderName, prev, upgradeTarget.Version)
+			GetContext(ctx).UpstreamProviderName, prev, upgradeTarget.Version)
 		for _, t := range upgradeTarget.GHIssues {
 			if t.Number > 0 {
 				fmt.Fprintf(b, "\tFixes #%d\n", t.Number)
 			}
 		}
 	}
-	if ctx.UpgradeBridgeVersion {
+	if GetContext(ctx).UpgradeBridgeVersion {
 		fmt.Fprintf(b, "- Upgrading pulumi-terraform-bridge from %s to %s.\n",
 			goMod.Bridge.Version, targetBridge)
 	}
-	if ctx.UpgradePfVersion {
+	if GetContext(ctx).UpgradePfVersion {
 		fmt.Fprintf(b, "- Upgrading pulumi-terraform-bridge/pf from %s to %s.\n",
 			goMod.Pf.Version, targetBridge)
 	}
 
-	if ctx.UpgradeSdkVersion {
+	if GetContext(ctx).UpgradeSdkVersion {
 		fmt.Fprintf(b, "- Upgrading pulumi SDK to `latest`.\n")
 	}
 	if parts := strings.Split(tfSDKUpgrade, " -> "); len(parts) == 2 {
@@ -169,7 +169,7 @@ func prBody(ctx Context, repo ProviderRepo,
 	return b.String()
 }
 
-func ensurePulumiRemote(ctx Context, name string) (string, error) {
+func ensurePulumiRemote(ctx context.Context, name string) (string, error) {
 	remotes, err := runGitCommand(ctx, func(b []byte) ([]string, error) {
 		return strings.Split(string(b), "\n"), nil
 	}, "remote")
@@ -198,7 +198,7 @@ func say(msg string) func([]byte) (string, error) {
 //
 // We don't use the current branch, since applying a partial update could change the current branch,
 // leading to a non idempotent result.
-func setCurrentUpstreamFromPatched(ctx Context, repo *ProviderRepo) error {
+func setCurrentUpstreamFromPatched(ctx context.Context, repo *ProviderRepo) error {
 	getCheckedInCommit := exec.CommandContext(ctx,
 		"git", "ls-tree", repo.defaultBranch, "upstream", "--object-only")
 	getCheckedInCommit.Dir = repo.root
@@ -258,13 +258,13 @@ func setCurrentUpstreamFromPatched(ctx Context, repo *ProviderRepo) error {
 //
 // We don't use the current branch, since applying a partial update could change the current branch,
 // leading to a non idempotent result.
-func setCurrentUpstreamFromPlain(ctx Context, repo *ProviderRepo, goMod *GoMod) error {
+func setCurrentUpstreamFromPlain(ctx context.Context, repo *ProviderRepo, goMod *GoMod) error {
 	return setUpstreamFromRemoteRepo(ctx, repo, "tags",
 		filepath.Join("provider", "go.mod"), goMod.Upstream.Path,
 		semver.NewVersion)
 }
 
-func setCurrentUpstreamFromForked(ctx Context, repo *ProviderRepo, goMod *GoMod) error {
+func setCurrentUpstreamFromForked(ctx context.Context, repo *ProviderRepo, goMod *GoMod) error {
 	return setUpstreamFromRemoteRepo(ctx, repo, "heads",
 		filepath.Join("provider", "go.mod"), goMod.Fork.New.Path,
 		func(s string) (*semver.Version, error) {
@@ -273,14 +273,14 @@ func setCurrentUpstreamFromForked(ctx Context, repo *ProviderRepo, goMod *GoMod)
 		})
 }
 
-func setCurrentUpstreamFromShimmed(ctx Context, repo *ProviderRepo, goMod *GoMod) error {
+func setCurrentUpstreamFromShimmed(ctx context.Context, repo *ProviderRepo, goMod *GoMod) error {
 	return setUpstreamFromRemoteRepo(ctx, repo, "tags",
 		filepath.Join("provider", "shim", "go.mod"), goMod.Upstream.Path,
 		semver.NewVersion)
 }
 
 func setUpstreamFromRemoteRepo(
-	ctx Context, repo *ProviderRepo, kind, goModPath, upstream string,
+	ctx context.Context, repo *ProviderRepo, kind, goModPath, upstream string,
 	parse func(string) (*semver.Version, error),
 ) error {
 	version, found, err := originalGoVersionOf(ctx, *repo, goModPath, upstream)
@@ -442,10 +442,10 @@ func getGitHubPath(repoPath string) (string, error) {
 // 2) current working directory: returns the path to the cwd if it is a provider directory
 // or subdirectory, i.e. `user/home/pulumi/pulumi-docker/provider` it
 // 3) default: $GOPATH/src/module, i.e. $GOPATH/src/github.com/pulumi/pulumi-datadog
-func getRepoExpectedLocation(ctx Context, cwd, repoPath string) (string, error) {
+func getRepoExpectedLocation(ctx context.Context, cwd, repoPath string) (string, error) {
 	// We assume the user passed in a valid path, either absolute or relative.
-	if ctx.repoPath != "" {
-		return ctx.repoPath, nil
+	if path := GetContext(ctx).repoPath; path != "" {
+		return path, nil
 	}
 
 	// Strip version
@@ -470,28 +470,28 @@ func getRepoExpectedLocation(ctx Context, cwd, repoPath string) (string, error) 
 		cwd = filepath.Dir(cwd)
 	}
 
-	return filepath.Join(ctx.GoPath, "src", expectedLocation), nil
+	return filepath.Join(GetContext(ctx).GoPath, "src", expectedLocation), nil
 }
 
 // Fetch the expected upgrade target from github. Return a list of open upgrade issues,
 // sorted by semantic version. The list may be empty.
 //
 // The second argument represents a message to describe the result. It may be empty.
-func GetExpectedTarget(ctx Context, name, upstreamOrg string) (*UpstreamUpgradeTarget, error) {
+func GetExpectedTarget(ctx context.Context, name, upstreamOrg string) (*UpstreamUpgradeTarget, error) {
 	// InferVersion == true: use issue system, with ctx.TargetVersion limiting the version if set
-	if ctx.InferVersion {
+	if GetContext(ctx).InferVersion {
 		return getExpectedTargetFromIssues(ctx, name)
 	}
-	if ctx.TargetVersion != nil {
-		return &UpstreamUpgradeTarget{Version: ctx.TargetVersion}, nil
+	if GetContext(ctx).TargetVersion != nil {
+		return &UpstreamUpgradeTarget{Version: GetContext(ctx).TargetVersion}, nil
 
 	}
 	return getExpectedTargetLatest(ctx, name, upstreamOrg)
 }
 
-func getExpectedTargetLatest(ctx Context, name, upstreamOrg string) (*UpstreamUpgradeTarget, error) {
+func getExpectedTargetLatest(ctx context.Context, name, upstreamOrg string) (*UpstreamUpgradeTarget, error) {
 	latest := exec.CommandContext(ctx, "gh", "release", "list",
-		"--repo="+upstreamOrg+"/"+ctx.UpstreamProviderName,
+		"--repo="+upstreamOrg+"/"+GetContext(ctx).UpstreamProviderName,
 		"--limit=1",
 		"--exclude-drafts",
 		"--exclude-pre-releases")
@@ -504,7 +504,7 @@ func getExpectedTargetLatest(ctx Context, name, upstreamOrg string) (*UpstreamUp
 
 	tok := strings.Fields(bytes.String())
 	contract.Assertf(len(tok) > 0, fmt.Sprintf("no releases found in %s/%s",
-		upstreamOrg, ctx.UpstreamProviderName))
+		upstreamOrg, GetContext(ctx).UpstreamProviderName))
 	v, err := semver.NewVersion(tok[0])
 	if err != nil {
 		return nil, err
@@ -512,7 +512,7 @@ func getExpectedTargetLatest(ctx Context, name, upstreamOrg string) (*UpstreamUp
 	return &UpstreamUpgradeTarget{Version: v}, nil
 }
 
-func getExpectedTargetFromIssues(ctx Context, name string) (*UpstreamUpgradeTarget, error) {
+func getExpectedTargetFromIssues(ctx context.Context, name string) (*UpstreamUpgradeTarget, error) {
 	target := &UpstreamUpgradeTarget{}
 	getIssues := exec.CommandContext(ctx, "gh", "issue", "list",
 		"--state=open",
@@ -561,7 +561,7 @@ func getExpectedTargetFromIssues(ctx Context, name string) (*UpstreamUpgradeTarg
 		return versions[j].Version.LessThan(versions[i].Version)
 	})
 
-	if ctx.TargetVersion != nil {
+	if ctx := GetContext(ctx); ctx.TargetVersion != nil {
 		var foundTarget bool
 		for i, v := range versions {
 			if v.Version.Equal(ctx.TargetVersion) {

upgrade/steps-helpers_test.go
@@ -1,6 +1,7 @@
 package upgrade
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -11,7 +12,7 @@ import (
 )
 
 func TestGetRepoExpectedLocation(t *testing.T) {
-	ctx := Context{
+	ctx := &Context{
 		GoPath: "/Users/myuser/go",
 	}
 
@@ -32,7 +33,7 @@ func TestGetRepoExpectedLocation(t *testing.T) {
 	for _, tt := range tests {
 		tt := tt
 		t.Run(fmt.Sprintf("(%s,%s,%s)", tt.cwd, tt.repoPath, tt.expected), func(t *testing.T) {
-			expected, err := getRepoExpectedLocation(ctx, tt.cwd, tt.repoPath)
+			expected, err := getRepoExpectedLocation(ctx.Wrap(context.Background()), tt.cwd, tt.repoPath)
 			expected = trimSeparators(expected)
 			assert.Nil(t, err)
 			assert.Equal(t, trimSeparators(tt.expected), expected)

upgrade/steps.go
@@ -42,7 +42,7 @@ func GitCommit(ctx context.Context, msg string) step.Step {
 // Upgrade the upstream fork of a pulumi provider.
 //
 // The SHA of the new upstream branch is returned.
-func upgradeUpstreamFork(ctx Context, name string, target *semver.Version, goMod *GoMod) step.Step {
+func upgradeUpstreamFork(ctx context.Context, name string, target *semver.Version, goMod *GoMod) step.Step {
 	var forkedProviderUpstreamCommit string
 	var upstreamPath string
 	var previousUpstreamVersion *semver.Version
@@ -110,7 +110,7 @@ func upgradeUpstreamFork(ctx Context, name string, target *semver.Version, goMod
 	).Return(&forkedProviderUpstreamCommit)
 }
 
-func ensureUpstreamRepo(ctx Context, repoPath string) step.Step {
+func ensureUpstreamRepo(ctx context.Context, repoPath string) step.Step {
 	var expectedLocation string
 	var repoExists bool
 	return step.Combined("Ensure '"+repoPath+"'",
@@ -158,14 +158,14 @@ func ensureUpstreamRepo(ctx Context, repoPath string) step.Step {
 	).Return(&expectedLocation)
 }
 
-var javaVersion *regexp.Regexp = regexp.MustCompile(`JAVA_GEN_VERSION := (v[0-9]+\.[0-9]+\.[0-9]+)`)
+var javaVersionRegexp *regexp.Regexp = regexp.MustCompile(`JAVA_GEN_VERSION := (v[0-9]+\.[0-9]+\.[0-9]+)`)
 
 func UpgradeProviderVersion(
-	ctx Context, goMod *GoMod, target *semver.Version,
+	ctx context.Context, goMod *GoMod, target *semver.Version,
 	repo ProviderRepo, targetSHA, forkedProviderUpstreamCommit string,
 ) step.Step {
 	steps := []step.Step{}
-	if ctx.JavaVersion != "" {
+	if javaVersion := GetContext(ctx).JavaVersion; javaVersion != "" {
 		var didChange bool
 		steps = append(steps, step.Combined("Update Java Version",
 			step.F("Current Java Version", func(cx context.Context) (string, error) {
@@ -173,22 +173,23 @@ func UpgradeProviderVersion(
 				if err != nil {
 					return "", err
 				}
-				found := javaVersion.FindSubmatch(b)
+				found := javaVersionRegexp.FindSubmatch(b)
 				if found == nil {
 					return "not found", nil
 				}
-				ctx.oldJavaVersion = string(found[1])
-				return ctx.oldJavaVersion, nil
+				oldJavaVersion := string(found[1])
+				GetContext(ctx).oldJavaVersion = oldJavaVersion
+				return oldJavaVersion, nil
 			}),
 			UpdateFileWithSignal("Update Makefile", "Makefile", &didChange,
 				func(b []byte) ([]byte, error) {
-					version := javaVersion.FindSubmatchIndex(b)
+					version := javaVersionRegexp.FindSubmatchIndex(b)
 					if version == nil {
 						return nil, fmt.Errorf("Java version set: could not find JAVA_GEN_VERSION")
 					}
 					var out bytes.Buffer
 					out.Write(b[:version[2]])
-					out.WriteString(ctx.JavaVersion)
+					out.WriteString(javaVersion)
 					out.Write(b[version[3]:])
 					return out.Bytes(), nil
 				}),
@@ -304,14 +305,14 @@ func UpgradeProviderVersion(
 }
 
 func InformGitHub(
-	ctx Context, target *UpstreamUpgradeTarget, repo ProviderRepo,
+	ctx context.Context, target *UpstreamUpgradeTarget, repo ProviderRepo,
 	goMod *GoMod, targetBridgeVersion, targetPfVersion, tfSDKUpgrade string,
 ) step.Step {
 	pushBranch := step.Cmd("git", "push", "--set-upstream",
 		"origin", repo.workingBranch).In(&repo.root)
 
 	var prTitle string
-	if ctx.UpgradeProviderVersion {
+	if ctx := GetContext(ctx); ctx.UpgradeProviderVersion {
 		prTitle = fmt.Sprintf("Upgrade %s to v%s",
 			ctx.UpstreamProviderName, target.Version)
 	} else if ctx.UpgradeBridgeVersion {
@@ -327,10 +328,10 @@ func InformGitHub(
 	}
 
 	createPR := step.Cmd("gh", "pr", "create",
-		"--assignee", ctx.PrAssign,
+		"--assignee", GetContext(ctx).PrAssign,
 		"--base", repo.defaultBranch,
 		"--head", repo.workingBranch,
-		"--reviewer", ctx.PrReviewers,
+		"--reviewer", GetContext(ctx).PrReviewers,
 		"--title", prTitle,
 		"--body", prBody(ctx, repo, target, goMod, targetBridgeVersion, tfSDKUpgrade),
 	).In(&repo.root)
@@ -340,7 +341,7 @@ func InformGitHub(
 		step.Computed(func() step.Step {
 			// If we are only upgrading the bridge, we wont have a list of
 			// issues.
-			if !ctx.UpgradeProviderVersion {
+			if !GetContext(ctx).UpgradeProviderVersion {
 				return nil
 			}
 
@@ -350,7 +351,7 @@ func InformGitHub(
 			for i, t := range target.GHIssues {
 				issues[i] = step.Cmd(
 					"gh", "issue", "edit", fmt.Sprintf("%d", t.Number),
-					"--add-assignee", ctx.PrAssign).In(&repo.root)
+					"--add-assignee", GetContext(ctx).PrAssign).In(&repo.root)
 			}
 			return step.Combined("Assign Issues", issues...)
 		}),
@@ -395,7 +396,7 @@ func setTFPluginSDKReplace(ctx context.Context, repo ProviderRepo, targetSHA *st
 	}).In(repo.providerDir())
 }
 
-func EnsureBranchCheckedOut(ctx Context, branchName string) step.Step {
+func EnsureBranchCheckedOut(ctx context.Context, branchName string) step.Step {
 	var branches string
 	var alreadyExists bool
 	var alreadyCurrent bool
@@ -432,11 +433,11 @@ func EnsureBranchCheckedOut(ctx Context, branchName string) step.Step {
 	)
 }
 
-func OrgProviderRepos(ctx Context, org, repo string) step.Step {
+func OrgProviderRepos(ctx context.Context, org, repo string) step.Step {
 	return ensureUpstreamRepo(ctx, path.Join("github.com", org, repo))
 }
 
-func PullDefaultBranch(ctx Context, remote string) step.Step {
+func PullDefaultBranch(ctx context.Context, remote string) step.Step {
 	var lsRemoteHeads string
 	var defaultBranch string
 	return step.Combined("pull default branch",
@@ -472,7 +473,7 @@ func PullDefaultBranch(ctx Context, remote string) step.Step {
 	).Return(&defaultBranch)
 }
 
-func MajorVersionBump(ctx Context, goMod *GoMod, target *UpstreamUpgradeTarget, repo ProviderRepo) step.Step {
+func MajorVersionBump(ctx context.Context, goMod *GoMod, target *UpstreamUpgradeTarget, repo ProviderRepo) step.Step {
 	if repo.currentVersion.Major() == 0 {
 		// None of these steps are necessary or appropriate when moving from
 		// version 0.x to 1.0 because Go modules only require a version suffix for
@@ -701,7 +702,7 @@ func UpdateFileWithSignal(desc, path string, didChange *bool, f func([]byte) ([]
 	})
 }
 
-func migrationSteps(ctx Context, repo ProviderRepo, providerName string, description string,
+func migrationSteps(ctx context.Context, repo ProviderRepo, providerName string, description string,
 	migrationFunc func(resourcesFilePath, providerName string) (bool, error)) ([]step.Step, error) {
 	steps := []step.Step{}
 	providerName = strings.TrimPrefix(providerName, "pulumi-")
@@ -727,7 +728,7 @@ func migrationSteps(ctx Context, repo ProviderRepo, providerName string, descrip
 	return steps, nil
 }
 
-func AddAutoAliasing(ctx Context, repo ProviderRepo) (step.Step, error) {
+func AddAutoAliasing(ctx context.Context, repo ProviderRepo) (step.Step, error) {
 	providerName := strings.TrimPrefix(repo.name, "pulumi-")
 	metadataPath := fmt.Sprintf("%s/cmd/pulumi-resource-%s/bridge-metadata.json", *repo.providerDir(), providerName)
 	steps := []step.Step{
@@ -752,7 +753,7 @@ func AddAutoAliasing(ctx Context, repo ProviderRepo) (step.Step, error) {
 	return step.Combined("Add AutoAliasing", steps...), nil
 }
 
-func ReplaceAssertNoError(ctx Context, repo ProviderRepo) (step.Step, error) {
+func ReplaceAssertNoError(ctx context.Context, repo ProviderRepo) (step.Step, error) {
 	steps, err := migrationSteps(ctx, repo, repo.name, "Remove deprecated contract.Assert",
 		AssertNoErrorMigration)
 	if err != nil {
@@ -764,7 +765,7 @@ func ReplaceAssertNoError(ctx Context, repo ProviderRepo) (step.Step, error) {
 // UpgradePulumiVersions reads the current Pulumi SDK version from go.mod and applies it to:
 // sdk/go.mod
 // examples/go.mod - we also infer the `pkg` version here and add it.
-func BridgePulumiVersions(ctx Context, repo ProviderRepo) step.Step {
+func BridgePulumiVersions(ctx context.Context, repo ProviderRepo) step.Step {
 	// When we've updated the bridge version, we need to update the corresponding pulumi version in sdk/go.mod.
 	// It needs to match the version used in provider/go.mod, which is *not* necessarily `latest`.
 	var newSdkVersion string

commit 32434d3b0eab70d06e855ab18f118e2f15ec899f

Disable PF upgrade when /pf is not found, not UpgradeProviderVersion
This regression was introduced in https://github.com/pulumi/upgrade-provider/pull/174.
Before #174, `Context` was passed by value, and calling `setFalse` didn't do anything. I
believe that code was always broken, but since it mutated a copy it was harmless.

This is a priority fix, since our automation is currently broken for providers that don't
use /pf.

upgrade/kind.go
@@ -104,9 +104,11 @@ func GetRepoKind(ctx context.Context, repo ProviderRepo) (*GoMod, error) {
 	pf, ok, err := originalGoVersionOf(ctx, repo, filepath.Join("provider", "go.mod"), "github.com/pulumi/pulumi-terraform-bridge/pf")
 	if err != nil {
 		return nil, err
-	}
-	if ctx := GetContext(ctx); !ok && ctx.UpgradeProviderVersion {
-		setFalse(&ctx.UpgradeProviderVersion)
+	} else if !ok {
+		// If we successfully opened provider/go.mod but didn't find any reference
+		// to "github.com/pulumi/pulumi-terraform-bridge/pf", we assume that the
+		// provider doesn't use /pf and it doesn't make sense to upgrade.
+		setFalse(&GetContext(ctx).UpgradePfVersion)
 	}
 
 	tfProviderRepoName := GetContext(ctx).UpstreamProviderName