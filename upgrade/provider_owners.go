fc025cac94e7987d132242dcebd6e717989fafa4
Ian Wahbe <ian@wahbe.com>
2023-12-19 10:28:13 -0800

    Add a mapping for archive (#223)

--- upgrade/provider_owners.go
+++ upgrade/provider_owners.go
@@ -3,6 +3,7 @@ package upgrade
 var ProviderOrgs = map[string]string{
 	"datadog": "DataDog",
 	"random":  "hashicorp",
+	"archive": "terraform-providers",
 }
 
 var ProviderName = map[string]string{
