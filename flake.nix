b50f4f45f2f84e1bb96cdc7fa7b051b89cdf16ce
Anton Tayanovskyy <anton@pulumi.com>
2024-11-09 00:09:01 -0500

    chore: update flake.nix to the newest Go version (#296)
    With these changes I can build this CLI again with Nix.

--- flake.nix
+++ flake.nix
@@ -2,20 +2,23 @@
   description = "A flake defining upgrade-provider build-from-source package";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-24.05;
   };
 
   outputs = { self, nixpkgs }: let
 
     package = { system }: let
       pkgs = import nixpkgs { system = system; };
-    in pkgs.buildGo121Module rec {
+    in pkgs.buildGo123Module rec {
       name = "upgrade-provider";
       version = ''${self.rev or "dirty"}'';
       src = ./.;
       doCheck = false;
-      vendorHash = "sha256-0InHprcsXT9I1foDSFKEXzmOxl7LC0FxRe7wOsv6BTo=";
+      vendorHash = "sha256-hllbzbfy1xa6q/3YXJOiDERsy8ELJlA/wwrrpsQ7r1k=";
       ldflags = [];
+      postInstall = ''
+        rm -rf $out/bin/generate
+      '';
     };
 
   in {
