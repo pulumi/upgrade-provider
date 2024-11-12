da77a8ed2a17678bd530f94247c6df87ac94f92c
Daniel Bradley <daniel@pulumi.com>
2024-11-12 14:27:33 +0000

    Set PR title before accessing it
    We were using the `repo.prTitle` before it was assigned a value which meant it was always an empty string while checking for existing PRs and therefore it never found an existing PR.

--- upgrade/upgrade_provider.go
+++ upgrade/upgrade_provider.go
@@ -184,6 +184,12 @@ func UpgradeProvider(ctx context.Context, repoOrg, repoName string) (err error) 
 		}
 	}
 
+	if prTitle, err := prTitle(ctx, upgradeTarget, targetBridgeVersion, targetPfVersion); err != nil {
+		return err
+	} else {
+		repo.prTitle = prTitle
+	}
+
 	var targetSHA string
 	err = stepv2.PipelineCtx(ctx, "Setup working branch", func(ctx context.Context) {
 		repo.workingBranch = getWorkingBranch(ctx, *GetContext(ctx), targetBridgeVersion, targetPfVersion, upgradeTarget)
@@ -192,12 +198,6 @@ func UpgradeProvider(ctx context.Context, repoOrg, repoName string) (err error) 
 	})
 	if err != nil {
 		return err
-	}
-
-	if prTitle, err := prTitle(ctx, upgradeTarget, targetBridgeVersion, targetPfVersion); err != nil {
-		return err
-	} else {
-		repo.prTitle = prTitle
 	}
 
 	if GetContext(ctx).MajorVersionBump {
