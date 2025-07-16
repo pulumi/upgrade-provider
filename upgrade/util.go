4041648764fe68c7d017a125bd7fb2527f366c1c
Anton Tayanovskyy <anton@pulumi.com>
2025-07-16 09:58:00 -0500

    Expose the ability to upgrade Java exclusively

--- upgrade/util.go
+++ upgrade/util.go
@@ -37,7 +37,8 @@ type Context struct {
 	UpgradeProviderVersion bool
 	MajorVersionBump       bool
 
-	UpgradeJavaVersion bool
+	UpgradeJavaVersion     bool
+	UpgradeJavaVersionOnly bool
 
 	// Some providers go for months without an upstream release, but do receive weekly bridge updates.
 	// upgrade-provider will detect if the provider's last release is more than eight weeks old, and if it is,
