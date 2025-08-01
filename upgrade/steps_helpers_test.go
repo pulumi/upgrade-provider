510b89e8faa60ec6d42a6f0df3e645ea607583e5
guineveresaenger <guinevere@pulumi.com>
2025-08-01 10:46:38 -0700

    Revert "Remove logic for current directory - it should always be the provider repo"
    This reverts commit 60c4b2b90fc02e8b497517378e3825a17c84affc.

--- upgrade/steps_helpers_test.go
+++ upgrade/steps_helpers_test.go
@@ -2,7 +2,9 @@ package upgrade
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
@@ -40,6 +42,36 @@ func TestRemoveVersionPrefix(t *testing.T) {
 		t.Run(tt.input, func(t *testing.T) {
 			actual := modPathWithoutVersion(tt.input)
 			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
+
+func TestGetRepoExpectedLocation(t *testing.T) {
+	ctx := &Context{
+		GoPath: "/Users/myuser/go",
+	}
+
+	mockRepoPath := filepath.Join("github.com", "pulumi", "random-provider")
+	defaultExpectedLocation := filepath.Join(ctx.GoPath, "src", mockRepoPath)
+
+	baseProviderCwd := string(os.PathSeparator) + filepath.Join("Users", "home", mockRepoPath)
+	subProviderCwd := filepath.Join(baseProviderCwd, "examples")
+	randomCwd := string(os.PathSeparator) + filepath.Join("Users", "random", "dir")
+
+	// test cwd == repo path
+	tests := []struct{ cwd, repoPath, expected string }{
+		{baseProviderCwd, mockRepoPath, baseProviderCwd},   // expected set to cwd
+		{subProviderCwd, mockRepoPath, baseProviderCwd},    // expected set to top level of cwd repo path
+		{randomCwd, mockRepoPath, defaultExpectedLocation}, // expected set to default on no match
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(fmt.Sprintf("(%s,%s,%s)", tt.cwd, tt.repoPath, tt.expected), func(t *testing.T) {
+			expected, err := getRepoExpectedLocation(ctx.Wrap(context.Background()), tt.cwd, tt.repoPath)
+			expected = trimSeparators(expected)
+			assert.Nil(t, err)
+			assert.Equal(t, trimSeparators(tt.expected), expected)
 		})
 	}
 }
